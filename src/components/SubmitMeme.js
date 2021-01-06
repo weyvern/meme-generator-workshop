e45687390398bb58795bb00a2484fb0e2a0a2640
Jorge Alberto Serrano Paul <Jorge Paul>
2021-01-06 18:51:48 +0100

    Submit

--- src/components/SubmitMeme.js
+++ src/components/SubmitMeme.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import { useContext } from 'react';
+import { MemeContext } from '../context/memeContext';
+import axios from 'axios';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const SubmitMeme = () => {
-  return <button>Generate</button>;
+  const { selectedMeme, setSelectedMeme, inputs } = useContext(MemeContext);
+
+  const querifyObj = obj => {
+    const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+    return '?' + params.join('&');
+  };
+
+  const generateMeme = () => {
+    const obj = {
+      username: process.env.REACT_APP_USERNAME,
+      password: process.env.REACT_APP_PASSWORD,
+      template_id: selectedMeme.id
+    };
+
+    const arr = inputs.map((v, idx) => `boxes[${idx}][text]=${v}`).join('&');
+
+    axios
+      .post(
+        `${process.env.REACT_APP_API}/caption_image${querifyObj(obj)}&${arr}`
+      )
+      .then(({ data: { data: { url } } }) =>
+        setSelectedMeme({ ...selectedMeme, url })
+      );
+  };
+
+  return (
+    <button className='btn btn-lg btn-success' onClick={generateMeme}>
+      Generate
+    </button>
+  );
 };
 
 export default SubmitMeme;
